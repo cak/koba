b3fdcb58e815399795e2da4f3dfb4004ed56b4ff
Caleb Kinney <cakinney@gmail.com>
2019-04-16 05:38:09 -0400

    Nest helpers and ReportT
    - nest helpers under Koba
    - nest ReportTo under CSP

--- Sources/Koba/Utils.swift
+++ Sources/Koba/Utils.swift
@@ -29,57 +29,65 @@ public struct KobaConfig {
     }
 }
 
-public struct Sources {
-    public static let data = "data:"
-    public static let none = "'none'"
-    public static let sameOrigin = "'self'"
-    public static let src = "'src'"
-    public static let strictDynamic = "'strict-dynamic'"
-    public static let unsafeEval = "'unsafe-eval'"
-    public static let unsafeInline = "'unsafe-inline'"
-    public static let wildcard = "*"
-}
+extension Koba {
+    public struct Source {
+        public static let data = "data:"
+        public static let mediastream = "mediastream:"
+        public static let https = "https:"
+        public static let blob = "blob:"
+        public static let filesystem = "filesystem:"
+        public static let none = "'none'"
+        public static let sameOrigin = "'self'"
+        public static let src = "'src'"
+        public static let strictDynamic = "'strict-dynamic'"
+        public static let unsafeEval = "'unsafe-eval'"
+        public static let unsafeInline = "'unsafe-inline'"
+        public static let wildcard = "*"
+    }
 
-public struct Seconds {
-    public static let fiveMinutes = "\(60 * 5)"
-    public static let oneDay = "\(60 * 60 * 24)"
-    public static let oneWeek = "\(60 * 60 * 24 * 7)"
-    public static let oneMonth = "\(60 * 60 * 24 * 30)"
-    public static let oneYear = "\(60 * 60 * 24 * 365)"
-    public static let twoYears = "\(60 * 60 * 24 * 365 * 2)"
-}
-
-public struct ReportTo: Codable {
-    private let group: String?
-    private let maxAge: Int
-    private let endpoints: [ReportToEndpoint]
-    private let includeSubdomains: Bool?
-
-    public init(group: String? = nil, maxAge: Int,
-                endpoints: [ReportToEndpoint], includeSubdomains: Bool? = nil) {
-        self.group = group
-        self.maxAge = maxAge
-        self.endpoints = endpoints
-        self.includeSubdomains = includeSubdomains
+    public struct Time {
+        public static let fiveMinutes = "\(60 * 5)"
+        public static let oneDay = "\(60 * 60 * 24)"
+        public static let oneWeek = "\(60 * 60 * 24 * 7)"
+        public static let oneMonth = "\(60 * 60 * 24 * 30)"
+        public static let oneYear = "\(60 * 60 * 24 * 365)"
+        public static let twoYears = "\(60 * 60 * 24 * 365 * 2)"
     }
 }
 
-public struct ReportToEndpoint: Codable {
-    private let url: String
+extension CSP {
+    public struct ReportTo: Codable {
+        private let group: String?
+        private let maxAge: Int
+        private let endpoints: [ReportToEndpoint]
+        private let includeSubdomains: Bool?
 
-    public init(url: String) {
-        self.url = url
+        public init(group: String? = nil, maxAge: Int,
+                    endpoints: [ReportToEndpoint], includeSubdomains: Bool? = nil) {
+            self.group = group
+            self.maxAge = maxAge
+            self.endpoints = endpoints
+            self.includeSubdomains = includeSubdomains
+        }
+    }
+
+    public struct ReportToEndpoint: Codable {
+        private let url: String
+
+        public init(url: String) {
+            self.url = url
+        }
     }
 }
 
-extension ReportToEndpoint: Equatable {
-    public static func == (lhs: ReportToEndpoint, rhs: ReportToEndpoint) -> Bool {
+extension CSP.ReportToEndpoint: Equatable {
+    public static func == (lhs: CSP.ReportToEndpoint, rhs: CSP.ReportToEndpoint) -> Bool {
         return lhs.url == rhs.url
     }
 }
 
-extension ReportTo: Equatable {
-    public static func == (lhs: ReportTo, rhs: ReportTo) -> Bool {
+extension CSP.ReportTo: Equatable {
+    public static func == (lhs: CSP.ReportTo, rhs: CSP.ReportTo) -> Bool {
         return lhs.group == rhs.group &&
             lhs.maxAge == rhs.maxAge &&
             lhs.endpoints == rhs.endpoints &&
